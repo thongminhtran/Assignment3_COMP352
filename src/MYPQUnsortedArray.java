3b2c0e14e913ac3dc0dafa49ef962ea1e1196db4
thytran142 <thytran142@hotmail.com>
2021-04-02 15:45:49 -0400

    Finish optimizing sorted MYPQ.

--- src/MYPQUnsortedArray.java
+++ src/MYPQUnsortedArray.java
@@ -1,11 +1,10 @@
 public class MYPQUnsortedArray implements MyPQ<Integer, String> {
     private Entry[] entries = new Entry[1];
-    private int nextPosition = 0;
-
+    private int realSize = 0;
 
     @Override
     public void insert(Integer k, String v) {
-        boolean isFullArray = this.entries[this.entries.length-1] != null;
+        boolean isFullArray = realSize == entries.length;
         if (isFullArray) {
             Entry[] temporaryEntries = new Entry[this.entries.length * 2];
             for (int i = 0; i < this.entries.length; i++) {
@@ -14,8 +13,8 @@ public class MYPQUnsortedArray implements MyPQ<Integer, String> {
             this.entries = temporaryEntries.clone();
         }
         Entry entry = new Entry(k, v);
-        this.entries[nextPosition] = entry;
-        nextPosition++;
+        this.entries[realSize] = entry;
+        realSize++;
     }
 
     @Override
@@ -44,70 +43,85 @@ public class MYPQUnsortedArray implements MyPQ<Integer, String> {
             return null;
         }
         int minPosition = this.getPositionByKey(minEntry.getK());
-      //  [1,2,3]
-        // index = 0;
-        // [2,3,null] // move 2,3
-        // [3,2,1] ==> [3,2,null] // move no one
-        // [3,1,2] ==> [3,2,null]// move 2
-        for(int i=minPosition;i< entries.length;i++){
-                // The last element will be null after removing the minimum entry
-                if(i == this.size()-1){
-                    entries[i] = null;
-                }
-                else if(i<entries.length-1){
-                    entries[i] = entries[i+1];
-                }
+        try{
+            this.removeByPosition(minPosition);
+            return minEntry;
+        }catch(Exception e) {
+            System.out.println("Exception " + e.getMessage());
+            return null;
         }
-        if(this.size()< entries.length/4){
-            Entry[] tempEntry = new Entry[entries.length/2];
-            for(int i=0;i< entries.length/2;i++){
-                tempEntry[i] = entries[i];
-            }
-            this.entries = tempEntry.clone();
-        }
-        nextPosition--;
-        return minEntry;
     }
 
+    /**
+     *
+     * @param position: Position we want to remove
+     * @throws Exception: If position passing is not valid or if array is empty
+     */
+    private void removeByPosition(int position) throws Exception {
+        // [3,1,2] ==> [3,2] ==> position = 1;
+        boolean isPositionValid = position < realSize && realSize > 0;
+        if (!isPositionValid) throw new Exception("Position " + position + " is more than the real size " + realSize);
+        int expectedSizeAfterRemovingItem = realSize - 1;
+        boolean isExpectedSizeAfterRemovingItemLessThan25PercentageCapacity = expectedSizeAfterRemovingItem < 0.4 * this.entries.length;
+        if (isExpectedSizeAfterRemovingItemLessThan25PercentageCapacity) {
+            expectedSizeAfterRemovingItem = expectedSizeAfterRemovingItem / 2;
+        }
+        Entry[] temporaryEntries = new Entry[expectedSizeAfterRemovingItem];
+        // Copy the first element
+        if (position == 0) {
+            // Remove first element
+            for (int index = 1; index < realSize; index ++) {
+                temporaryEntries[index - 1] = this.entries[index];
+            }
+        } else {
+            int temporaryEntriesIndex = 0;
+            // Copy the beginning
+            for (int index = 0; index < position - 1; index ++) {
+                temporaryEntries[temporaryEntriesIndex] = this.entries[index];
+                temporaryEntriesIndex++;
+            }
+            // Copy the rest
+            boolean isLastElement = position == realSize - 1;
+            if (!isLastElement) {
+                for (int index = position + 1; index < realSize; index ++) {
+                    temporaryEntries[temporaryEntriesIndex] = this.entries[index];
+                    temporaryEntriesIndex++;
+                }
+            }
+        }
+        this.entries = temporaryEntries.clone();
+        realSize --; // Update real size
+    }
+
+    /**
+     *
+     * @param key: the key given to get the index of an entry in an array
+     * @return the index of the entry, return -1 when we cannot find the entry
+     */
     private int getPositionByKey(Integer key) {
         for (int i = 0; i < entries.length; i++) {
             if (entries[i] == null) {
                 break;
             }
-            if(entries[i].getK()==key){
+            if (entries[i].getK().equals(key)) {
                 return i;
             }
         }
         return -1;
     }
-
+    @Override
+    /**
+     * @return size of the array
+     */
     public int size() {
-        int size = 0;
-        for (int i = 0; i < entries.length; i++) {
-            if (entries[i] != null)
-                size++;
-        }
-        return size;
+        return realSize;
     }
 
     @Override
+    /**
+     * @return boolean true if an array is empty
+     */
     public boolean isEmpty() {
-        return nextPosition == 0;
-    }
-
-    public void printArray(){
-        for(int i=0;i<this.size();i++){
-            System.out.println(entries[i].getK()+" "+entries[i].getV());
-        }
-    }
-
-
-
-    public Entry[] getEntries() {
-        return entries;
-    }
-
-    public void setEntries(Entry[] entries) {
-        this.entries = entries;
+        return realSize == 0;
     }
 }
